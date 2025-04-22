9a92f9b0038f40a4316d53ca53cfde1f7fa43b36
AlaaQparts <alaa.khedr196@gmail.com>
2025-04-22 15:19:39 +0200

    Add files via upload

---
+++ Lora-normal.js
@@ -0,0 +1,7 @@
+﻿import { jsPDF } from "jspdf"
+var font = 'undefined';
+var callAddFont = function () {
+this.addFileToVFS('Lora-normal.ttf', font);
+this.addFont('Lora-normal.ttf', 'Lora', 'normal');
+};
+jsPDF.API.events.push(['addFonts', callAddFont])
